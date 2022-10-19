1774004ca3423d72ec0a676a27e9d043d9da9949
igorcuric44 <igor.curictt@gmail.com>
2022-10-19 17:02:48 +0200

    new comment

--- index.js
+++ index.js
@@ -12,6 +12,25 @@ app.get('/api/courses',(req,res)=>
 	res.send([1,2,3]);
 });
 
+/*
+app.get('/api/posts/:id',(req,res)=>
+{
+	res.send(req.params.id);
+});
+*/
+
+app.get('/api/posts/:year/:month',(req,res)=>
+{
+	res.send(req.params);
+});
+
+
+
+
+
+
+
+
 //port
 const port=process.env.PORT || 3000;
 
