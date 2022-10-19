e68b0e466f48cfa0a90fd039595d979bcc7e4fe1
igorcuric44 <igor.curictt@gmail.com>
2022-10-20 01:26:47 +0200

    new commit again

---
+++ index6.js
@@ -0,0 +1,18 @@
+var express=require('express');
+var app=express();
+
+app.use(express.static('public'));
+
+app.get('/',function(req,res)
+{
+	res.send('Hello Worldii');
+});
+
+var server=app.listen(8081,function()
+{
+	var host=server.address().address;
+	var port=server.address().port;
+
+	console.log("Example app listening at http://%s:%s",host,port);
+
+});
